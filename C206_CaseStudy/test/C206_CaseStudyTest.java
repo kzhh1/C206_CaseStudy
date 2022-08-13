f75838af500b1245c39f9937c80a2c2a9249bfcd
cheng <cheng@LAPTOP-7BFJTCQM>
2022-08-13 14:48:17 +0800

    committed by hui hui (adjusting testcodes)

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,17 +1,145 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	
+	// Chin Wei : 
+	
+	
+	// Desmond : 
+	
+	
+	// Zhen Hong : 
+	
+	
 
+	// Hui Hui 
+	private LunchBoxOrder lb1 ; 
+	private LunchBoxOrder lb2 ; 
+	
+	private ArrayList <LunchBoxOrder> lbOrderList ; 
+	
+	// Shi Hao : 
+	
+	
+
+	public C206_CaseStudyTest() { 
+		super() ; 
+	}
+	
+	
+	
 	@Before
 	public void setUp() throws Exception {
-	}
+	// Prepare test data 
+		
+		// Chin Wei : 
+		
+		
+		// Desmond : 
+		
+		
+		// Zhen Hong : 
+		
+		
 
+		// Hui Hui : 
+		
+		lb1 = new LunchBoxOrder("lb001" , "Chicken Chop", "Bandung", "Mango") ;
+		lb2 = new LunchBoxOrder("lb002", "chicken Curry", "Teh", "Watermelon") ;
+		
+		
+		lbOrderList = new ArrayList<LunchBoxOrder>() ; 
+		// Shi hao :
+		
+
+		
+	}
+	
+// ==================== test Viewing of classes ====================
+	
+	
+// ==================== test Adding of classes ====================
+	
+	
+	// Chin Wei : 
+	@Test 
+	public void testAddAccount() { 
+		
+		
+	}
+	// Desmond : 
+	@Test
+	public void testAddMenu() { 
+		
+		
+	}
+	// Zhen Hong : 
+	@Test
+	public void testAddMonthlyMenu() { 
+		
+	}
+	//Hui Hui : 
+	@Test 
+	public void testAddLunchBoxOrder() { 
+		
+		
+	}
+	// Shi Hao : 
+	@Test 
+	public void testAddOrderBill() { 
+		
+	}
+	
+	
+	
+// ==================== test Deleting of classes ====================
+	
+	// Chin Wei : 
+	
+	
+	// Desmond : 
+	
+	
+	// Zhen Hong : 
+	
+	
+	// Hui Hui - 
+	@Test 
+	public void testDeletelbOrder() { 
+		
+	}
+	
+	// Shi Hao : 
+	
+	
+	
+	
+	
 	@After
 	public void tearDown() throws Exception {
+		// Chin Wei : 
+		
+		
+		// Desmond : 
+		
+		
+		// Zhen Hong : 
+		
+		
+
+		// Hui Hui : 
+		lb1 = null ;
+		lb2 = null ; 
+		lbOrderList = null ; 
+		
+		
+		// Shi hao :
 	}
 
 	@Test
